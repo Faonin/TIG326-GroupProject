782c6a761cb4c52c23e730aa2f5f9d0c81eeea10
Anton Nilsson <anton.smeliy@gmail.com>
2024-04-10 12:20:30 +0200

    Updated to download data from 2020 and forward

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import requests
+import json
+from zipfile import ZipFile 
 
-response = requests.get("https://data.jobtechdev.se/annonser/historiska/2023.json") 
-print(response)
+
+the_working_year = 2020
+
+while requests.head("https://data.jobtechdev.se/annonser/historiska/" + str(the_working_year) + ".jsonl.zip").status_code == 200:
+    response = requests.get("https://data.jobtechdev.se/annonser/historiska/" + str(the_working_year) + ".jsonl.zip") 
+
+    temp = open("data.zip", 'wb')
+    temp.write(response.content) 
+
+    with ZipFile('data.zip', 'r') as zip:
+        print(zip.read(str(the_working_year) + ".jsonl"))
+    
+    the_working_year += 1
+
