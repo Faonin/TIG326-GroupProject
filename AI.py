ce504d904513bfce968b01a80f8d8a8c1ac7ad8f
Anton Nilsson <anton.smeliy@gmail.com>
2024-04-23 11:52:37 +0200

    Weighted scores

--- AI.py
+++ AI.py
@@ -24,7 +24,7 @@ with open("data/data.json", 'r', encoding="utf-8") as dataset:
             break
 
 
-vectorizer = CountVectorizer(stop_words=all_stopwords, token_pattern=r'\b[a-zA-ZåäöÅÄÖ]+\b', ngram_range=(1, 2))
+vectorizer = CountVectorizer(stop_words=all_stopwords, token_pattern=r'\b[a-zA-ZåäöÅÄÖ]+\b', ngram_range=(1, 3))
 
 x = vectorizer.fit_transform(data)
 
@@ -36,7 +36,24 @@ corpus = Sparse2Corpus(x, documents_columns=False)
 # Create id to word dictionary
 id2word = {i: word for i, word in enumerate(feature_names)}
 
-lda_model = gensim.models.LdaModel(corpus=corpus, num_topics=125, id2word=id2word, passes=20, random_state=42)
+
+adjusted_corpus = []
+for doc in corpus:
+    new_doc = []
+    for word_id, freq in doc:
+        if id2word[word_id] in ["kunskap i", "erfarenhet av", "skicklig på", "tränad i", "bekant med",
+        "kompetent inom", "expertis inom", "kvalificerad i", "certifierad i",
+        "behörig inom", "effektiv på", "färdig med", "sakkunnig i", "utbildad i",
+        "specialiserad på","proficient in", "knowledge of", "skilled in", "experienced with",
+        "trained in", "familiar with", "competent in", "expertise in",
+        "qualified in", "capable of", "adept in", "accomplished in",
+        "specialized in", "certified in", "efficient in"]:
+            freq *= 10  # Increase their frequency to give them more weight
+        new_doc.append((word_id, freq))
+    adjusted_corpus.append(new_doc)
+
+
+lda_model = gensim.models.LdaModel(corpus=adjusted_corpus, num_topics=125, id2word=id2word, passes=25, random_state=42, iterations=5)
 
 
 theText = open("data/text.txt", 'a', encoding="utf-8")
@@ -45,3 +62,4 @@ theText = open("data/text.txt", 'a', encoding="utf-8")
 topics = lda_model.print_topics()
 for topic in topics:
     theText.write(str(topic) + "\n")
+ 
